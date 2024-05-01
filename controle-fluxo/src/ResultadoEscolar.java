6812c60e3f2ada903564effb12c8ba220a9932c4
Natal Lima <nataldelima@gmail.com>
2024-04-30 21:37:53 -0400

    Incluida ckass de PlanoOperadora pra estudar switch case

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,9 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 7;
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Prova recuperação");
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
 
-        else
-            System.out.println("Reprovado");
+        System.out.println(resultado);
     }
 }
